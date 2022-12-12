1b4597d68539f2a51c4edf77e6a0b1b8e9e022ea
HyeWon Cho <5876675@gmail.com>
2022-12-13 01:54:53 +0900

    <feat> : Hand Sign Data Store

--- H2KE/H2E/handSignDetection.py
+++ H2KE/H2E/handSignDetection.py
@@ -1,58 +1,59 @@
 import cv2
-from cvzone .HandTrackingModule import HandDetector
+from cvzone.HandTrackingModule import HandDetector
 import numpy as np
 import math
 import time
 
 cap = cv2.VideoCapture(0)
-detector = HandDetector(mode=False, maxHands=2, detectionCon=0.5, minTrackCon=0.5)
+detector = HandDetector(maxHands=1)
 
 offset = 20
-imgsize = 300
+imgSize = 300
 
-folder = "Data/A"  # A라는 폴더에 저장할꺼임,(폴더 바꿔주면서 데이터 저장)
+folder = "H2KE\Data\C" # 데이터 저장할 폴더 A, B, C로 매번 바꿔준 후 S 눌러서 폴더에 저장하는 방식
 counter = 0
 
 while True:
     success, img = cap.read()
-    Hands, img = detector.findHands(img)
-    if Hands:
+    hands, img = detector.findHands(img)
+    if hands:
         # 이미지 분할
         hand = hands[0]
         x, y, w, h = hand['bbox']
 
-        imgWhite = np.ones((imgSize, imgsize, 3), np.uint8)*255
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8) * 255
+        imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
 
-        imgCrop = img[y-offset:y+h+offset, x-offset:x+w+offset]
+        imgCropShape = imgCrop.shape
 
-        imgCropshape = imgCrop.shape
-        imgWhite[0:imgCropshape[0], 0:imgCropshape[1]] = imgCrop
+        aspectRatio = h / w
 
-        aspectRatio = h/w
 
-        if aspectRatio >1 :
-            k = imgsize/h
-            wCal = math.cell(w+k)
-            imgResize = cv2.resize(imgCrop, (wCal, imgsize))
-            imgReshape = imgResize.shape
-            wGap = math.cell((300-wCal)/2)
-            imgWhite[:, wGap:wCal+wGap] = imgResize
+
+        # 일정 사이즈를 넘어가면 에러가나서 웹캠이 멈춤 -> resize때문!
+        # 웹캠에서 조금 멀리 떨어져서 손 인식하는 것이 에러 안 남!
+        if aspectRatio > 1:
+            k = imgSize / h
+            wCal = math.ceil(k * w)
+            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+            imgResizeShape = imgResize.shape
+            wGap = math.ceil((imgSize - wCal) / 2)
+            imgWhite[:, wGap:wCal + wGap] = imgResize
+
         else:
-            k = imgsize / w
-            hCal = math.cell(h + k)
-            imgResize = cv2.resize(imgCrop, (imgsize, hCal))
-            imgReshape = imgResize.shape
-            hGap = math.cell((300 - hCal) / 2)
-            imgWhite[wGap:wCal + wGap, :] = imgResize
+            k = imgSize / w
+            hCal = math.ceil(k * h)
+            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+            imgResizeShape = imgResize.shape
+            hGap = math.ceil((imgSize - hCal) / 2)
+            imgWhite[hGap:hCal + hGap, :] = imgResize
 
+        cv2.imshow("ImageCrop", imgCrop)
+        cv2.imshow("ImageWhite", imgWhite)
 
-        cv2.imshow('Hand', imgCrop)
-        cv2.imshow('Hand', imgWhite)
-
-
-    cv2.imshow('Hand', img)
-    key = cv2.waitkey(1)
-    if key == ord("s"): # s 누르면 데이터 저장
+    cv2.imshow("Image", img)
+    key = cv2.waitKey(1)
+    if key == ord("s"):
         counter += 1
-        cv2.imwrite(f'{folder}/image_{time.time()}.jpg', imgWhite)
+        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg', imgWhite)
         print(counter)
